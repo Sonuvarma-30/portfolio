79c00ae1d258adac512d15d0c690f266b8fe284a
ikshitha19 <ikshitha2005@gmail.com>
2026-09-25 20:11:13 +0530

    Rename Download Resume button to Resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -205,7 +205,7 @@ export default function Hero() {
                 <polyline points="7 10 12 15 17 10" />
                 <line x1="12" y1="15" x2="12" y2="3" />
               </svg>
-              Download Resume
+              Resume
             </a>
 
             {/* GitHub */}
